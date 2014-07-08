51142068fd84732a6abd63c4c9c096ee4f5e0272
Manfred Endres <manfred.endres@tslarusso.de>
2014-07-08 13:49:03 +0200

    try

--- lib/client.rb
+++ lib/client.rb
@@ -1,6 +1,7 @@
 require 'trello'
 
 Trello.configure do |config|
+  puts ENV.fetch('TRELLO_KEY')
   config.developer_public_key = ENV.fetch('TRELLO_KEY')
   config.member_token = ENV.fetch('TRELLO_MEMBER_TOKEN')
 end
